85b76bc172aa13bf50eedc85642b9e32b663b874
Emil <s195383@student.dtu.dk>
2020-10-26 05:06:09 +0100

    Flere layouts

--- app/src/main/java/com/example/brintaudientes/MainActivity.java
+++ app/src/main/java/com/example/brintaudientes/MainActivity.java
@@ -3,14 +3,13 @@ package com.example.brintaudientes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.music_player);
         //Toast.makeText(this, "hej", Toast.LENGTH_SHORT).show();
     }
 }
