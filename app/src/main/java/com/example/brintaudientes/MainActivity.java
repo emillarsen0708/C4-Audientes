038256ec644b52d67879bd1035c762acb88d5f55
Emil <s195383@student.dtu.dk>
2021-01-16 19:56:17 +0100

    Når der trykkes på + knapper på PresetFragment ændres drawable til preset_add_rectangle.xml. Nye knapper vises først, når forrige er trykket

--- app/src/main/java/com/example/brintaudientes/MainActivity.java
+++ app/src/main/java/com/example/brintaudientes/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.brintaudientes;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelStoreOwner;
 
