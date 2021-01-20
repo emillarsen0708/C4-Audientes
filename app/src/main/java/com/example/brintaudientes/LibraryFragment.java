8a772de43d8b1959fefef8d56dd504d5ee736b11
haida <Haidariano@hotmail.com>
2021-01-20 20:13:59 +0100

    hardcorded strings er blevet rettet, der mangler stadig en del

--- app/src/main/java/com/example/brintaudientes/LibraryFragment.java
+++ app/src/main/java/com/example/brintaudientes/LibraryFragment.java
@@ -58,6 +58,9 @@ public class LibraryFragment extends Fragment {
             mediaPlayer = MediaPlayer.create(getActivity(), resId);
             mediaPlayer.start();
 
+
+
+
         });
 
         return root;
